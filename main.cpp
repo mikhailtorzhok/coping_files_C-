2c3d8d93681799f742e0bf1687dbed2ad04e2f40
unknown <mikhailtorzhok@gmail.com>
2022-12-03 13:58:11 +0300

    initial commit2

--- main.cpp
+++ main.cpp
@@ -1,54 +1,55 @@
 #include <stdio.h>
-#include <iostream>
 #include <errno.h>
+#include <sstream>
+#include <string.h>
 #define BUF_SIZE 256
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-     cout << "dd if= of= ";
-     cout << endl;
+    FILE *in_file, *out_file;
+    char rec [BUF_SIZE];
 
-    // FILE *in_file, *out_file;
+    size_t bytes_in, bytes_out;
 
-/*    char rec [BUF_SIZE];
+    stringstream strs;
+    strs << argc;
+    string temp_str = strs.str();
+    char* char_type = (char*) temp_str.c_str();
+    printf(char_type);
 
-     size_t bytes_in, bytes_out;
-
-    if (argc != 3) {
+    if (argc != 3) {
         printf("Использование: срС файл1 файл2 ");
-        return 1;
-    }
+        return 1;
+    }
 
-    in_file = fopen(argv [1], "rb");
+    in_file = fopen(argv [1], "rb");
 
-    if (in_file == NULL) {
-        perror(argv[1]);
-        return 2;
-    }
+    if (in_file == NULL) {
+        perror(argv[1]);
+        return 2;
+    }
 
-    out_file = fopen(argv [2], "wb");
+    out_file = fopen(argv [2], "wb");
 
-    if (out_file == NULL) {
-    perror(argv [2]);
-    return 3;
-    }
+    if (out_file == NULL) {
+    perror(argv [2]);
+    return 3;
+    }
 
-    
-    while ((bytes_in = fread(rec, 1, BUF_SIZE, in_file)) > 0) {
-        bytes_out = fwrite(rec, 1, bytes_in, out_file);
+    /* Обработать входной файл по одной записи за один раз. */
+    while ((bytes_in = fread(rec, 1, BUF_SIZE, in_file)) > 0) {
+        bytes_out = fwrite(rec, 1, bytes_in, out_file);
 
-        if (bytes_out != bytes_in) {
+        if (bytes_out != bytes_in) {
             perror("Неустранимая ошибка записи.");
             return 4;
         }
 
-    }
+    }
 
-    fclose (in_file);
-    fclose (out_file);
-*/    
+    fclose (in_file);
+    fclose (out_file);
     return 0;
-
 }
